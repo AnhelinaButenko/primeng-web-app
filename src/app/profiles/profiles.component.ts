6e3cea544a84d2558c7f347e3eb002f879eff97d
Anhelina Butenko <angelinaa2712@gmail.com>
2024-04-01 23:14:40 +0300

    start crated page for profile

--- src/app/profiles/profiles.component.ts
+++ src/app/profiles/profiles.component.ts
@@ -1,9 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {Observable, throwError} from "rxjs";
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {IProfile} from "./profiles";
 import {ProfilesService} from "./profiles.service";
-import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-profiles',
@@ -40,10 +38,26 @@ export class ProfilesComponent implements OnInit {
   }
 
   onSubmit(): void {
+
+    console.log(JSON.stringify(this.form?.value));
+
+    const profile: IProfile = {
+      id: this.form?.value.id ?? 0,
+      userName: this.form?.value.userName ?? "",
+      email: this.form?.value.email ?? "",
+      currentWeight: this.form?.value.currentWeight ?? 0,
+      desiredWeight: this.form?.value.desiredWeight ?? 0,
+      height: this.form?.value.height ?? 0,
+      age: this.form?.value.age ?? 0,
+      gender: this.form?.value.gender ?? "",
+      activityLevel: this.form?.value.activityLevel ?? "",
+    }
+
     if (this.user) {
       this.user.activityLevel = this.selectedActivityLevel;
       this.user.gender = this.selectedGenderOption;
-      this.profileService.updateProfile(this.user.id, this.user).subscribe(
+
+      this.profileService.updateProfile(this.user.id, profile).subscribe(
         (updatedProfile: IProfile) => {
           console.log('Profile updated successfully:', updatedProfile);
           this.user = updatedProfile;
@@ -53,166 +67,41 @@ export class ProfilesComponent implements OnInit {
         }
       );
     }
-    //     console.log(JSON.stringify(this.form?.value));
-//
-//     const profile: IProfile = {
-//       id: this.form?.value.id ?? 0,
-//       userName: this.form?.value.userName ?? "",
-//       email: this.form?.value.email ?? "",
-//       currentWeight: this.form?.value.currentWeight ?? 0,
-//       desiredWeight: this.form?.value.desiredWeight ?? 0,
-//       height: this.form?.value.height ?? 0,
-//       age: this.form?.value.age ?? 0,
-//       gender: this.form?.value.gender ?? "",
-//       activityLevel: this.form?.value.activityLevel ?? "",
-//     }
-//
-//     if (this.form?.value.id) {
-//       this.updateSub = this.profileService.updateProfile(this.form?.value.id, profile).subscribe({
-//         next: profile => {
-//           console.log(profile);
-//           this.messageService.add({severity:'success', summary:'Service Message', detail:'This data is updated'});
-//         }
-//       })
-//     }
   }
 
   ngOnInit(): void {
     this.profileService.getById(1).subscribe((profile: IProfile) => {
       this.user = profile;
-    });
-    //     if(id) {
-//       this.getByIdSub = this.profileService.getById(id).subscribe({
-//         next: (profile: IProfile) => {
-//           this.profile = profile;
-//
-//           this.form = this.fb.group({
-//             id: [profile.id],
-//             userName: [profile.userName, [Validators.required]],
-//             email: [profile.email, [Validators.required]],
-//             currentWeight: [profile.currentWeight, [Validators.required]],
-//             desiredWeight: [profile.desiredWeight, [Validators.required]],
-//             height: [profile.height, [Validators.required]],
-//             age: [profile.age, [Validators.required]],
-//             gender: [profile.gender, [Validators.required]],
-//             activityLevel: [profile.activityLevel, [Validators.required]],
-//           });
-//         }
-//       });
-//     }
-//     else {
-//       this.form = this.fb.group({
-//         id: [0],
-//         userName: [undefined, [Validators.required]],
-//         email: [undefined, [Validators.required]],
-//         currentWeight: [undefined, [Validators.required]],
-//         desiredWeight: [undefined, [Validators.required]],
-//         height: [undefined, [Validators.required]],
-//         age: [undefined, [Validators.required]],
-//         gender: [undefined, [Validators.required]],
-//         activityLevel: [undefined, [Validators.required]],
-//       });
-//     }
-  }
 
-  protected readonly undefined = undefined;
+            this.form = this.fb.group({
+            id: [profile.id],
+            userName: [profile.userName, [Validators.required, Validators.minLength(2), Validators.max(50)]],
+            email: [profile.email, [Validators.required, Validators.email]],
+            currentWeight: [profile.currentWeight, [Validators.required, Validators.minLength(1), Validators.max(300)]],
+            desiredWeight: [profile.desiredWeight, [Validators.required, Validators.minLength(1), Validators.max(300)]],
+            height: [profile.height, [Validators.required, Validators.minLength(50), Validators.max(50)]],
+            age: [profile.age, [Validators.required, Validators.minLength(1), Validators.max(150)]],
+            gender: [profile.gender, [Validators.required]],
+            activityLevel: [profile.activityLevel, [Validators.required]],
+          });
+
+      // this.selectedGenderOption = profile.gender as number;
+      // this.selectedActivityLevel = profile.activityLevel as number;
+
+          if(!this.user?.id) {
+            this.form = this.fb.group({
+            id: [0],
+            userName: [undefined, [Validators.required, Validators.minLength(2), Validators.max(50)]],
+            email: [undefined, [Validators.required, Validators.email]],
+            currentWeight: [undefined, [Validators.required, Validators.minLength(1), Validators.max(300)]],
+            desiredWeight: [undefined, [Validators.required, Validators.minLength(1), Validators.max(300)]],
+            height: [undefined, [Validators.required, Validators.minLength(50), Validators.max(250)]],
+            age: [undefined, [Validators.required, Validators.minLength(1), Validators.max(150)]],
+            gender: [undefined, [Validators.required]],
+            activityLevel: [undefined, [Validators.required]],
+      });
+    }
+    });
+  }
 }
 
-// export class ProfilesComponent implements OnInit, OnDestroy {
-
-//   sub: Subscription | undefined;
-//   getByIdSub: Subscription | undefined;
-//   updateSub: Subscription | undefined;
-//   profile:  IProfile | undefined;
-//   profilesList: IProfile[] = [];
-//
-//   form: FormGroup| undefined = undefined;
-//
-//   recommendedCalories: number = 0;
-//
-//   constructor(private fb: FormBuilder,
-//               private route: ActivatedRoute,
-//               private router: Router,
-//               private messageService: MessageService,
-//               private profileService: ProfilesService) {
-//   }
-//
-//   ngOnInit(): void {
-//
-//     // this.profileService.getProfiles().subscribe((profiles: IProfile[]) => {
-//     //     this.profilesList = profiles;
-//     // });
-//
-//     const id = Number(this.route.snapshot.paramMap.get('id'));
-//
-//     if(id) {
-//       this.getByIdSub = this.profileService.getById(id).subscribe({
-//         next: (profile: IProfile) => {
-//           this.profile = profile;
-//
-//           this.form = this.fb.group({
-//             id: [profile.id],
-//             userName: [profile.userName, [Validators.required]],
-//             email: [profile.email, [Validators.required]],
-//             currentWeight: [profile.currentWeight, [Validators.required]],
-//             desiredWeight: [profile.desiredWeight, [Validators.required]],
-//             height: [profile.height, [Validators.required]],
-//             age: [profile.age, [Validators.required]],
-//             gender: [profile.gender, [Validators.required]],
-//             activityLevel: [profile.activityLevel, [Validators.required]],
-//           });
-//         }
-//       });
-//     }
-//     else {
-//       this.form = this.fb.group({
-//         id: [0],
-//         userName: [undefined, [Validators.required]],
-//         email: [undefined, [Validators.required]],
-//         currentWeight: [undefined, [Validators.required]],
-//         desiredWeight: [undefined, [Validators.required]],
-//         height: [undefined, [Validators.required]],
-//         age: [undefined, [Validators.required]],
-//         gender: [undefined, [Validators.required]],
-//         activityLevel: [undefined, [Validators.required]],
-//       });
-//     }
-//
-//   }
-//
-//   onSubmit(): void {
-//     console.log(JSON.stringify(this.form?.value));
-//
-//     const profile: IProfile = {
-//       id: this.form?.value.id ?? 0,
-//       userName: this.form?.value.userName ?? "",
-//       email: this.form?.value.email ?? "",
-//       currentWeight: this.form?.value.currentWeight ?? 0,
-//       desiredWeight: this.form?.value.desiredWeight ?? 0,
-//       height: this.form?.value.height ?? 0,
-//       age: this.form?.value.age ?? 0,
-//       gender: this.form?.value.gender ?? "",
-//       activityLevel: this.form?.value.activityLevel ?? "",
-//     }
-//
-//     if (this.form?.value.id) {
-//       this.updateSub = this.profileService.updateProfile(this.form?.value.id, profile).subscribe({
-//         next: profile => {
-//           console.log(profile);
-//           this.messageService.add({severity:'success', summary:'Service Message', detail:'This data is updated'});
-//         }
-//       })
-//     }
-//   }
-//
-//   // calculateRecommendedCalories(): void {
-//   //
-//   //   this.recommendedCalories =
-//   // }
-//
-//   ngOnDestroy(): void {
-//     this.sub?.unsubscribe();
-//     this.getByIdSub?.unsubscribe();
-//     this.updateSub?.unsubscribe();
-//   }
-// }
